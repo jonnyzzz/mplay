ba9130ec37775a6c23cdb54260a013fa07ff2329
Eugene Petrenko <me@jonnyzzz.com>
2021-01-09 14:13:03 +0100

    include agent-runtime to publication

--- platform/agent-runtime/build.gradle.kts
+++ platform/agent-runtime/build.gradle.kts
@@ -1,7 +1,29 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     kotlin("jvm")
+    `java-library`
+    `maven-publish`
 }
 
 dependencies {
     implementation(project(":config"))
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
